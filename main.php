388d1108bae38cba4759039888df45dfd7849998
mambaJumba <yuioyuio2004@mail.ru>
2020-01-22 18:59:49 +0300

    commit 2

--- main.php
+++ main.php
@@ -1,16 +1,21 @@
 <?php
 
-
 class Animal
 {
     private static $counter = 0;
     private $id;
+    public $typeAnimal;
+    public $typeProduct;
     protected $min;
     protected $max;
 
-    public function __construct()
+    public function __construct($typeAnimal, $typeProduct, $min, $max)
     {
         $this->id = self::$counter++;
+        $this->typeAnimal = $typeAnimal;
+        $this->typeProduct = $typeProduct;
+        $this->min = $min;
+        $this->max = $max;
     }
 
     public function work()
@@ -19,61 +24,33 @@ class Animal
     }
 }
 
-class Cow extends Animal
-{
-    protected $min = 8;
-    protected $max = 12;
-}
-
-class Chicken extends Animal
-{
-    protected $min = 0;
-    protected $max = 1;
-}
-
 class Farm
 {
-    public $cows = [];
-    protected $chickens = [];
+    protected $animals = [];
 
-    protected $milk = 0;
-    protected $eggs = 0;
-
-    public function setCows($count)
+    public function setAnimals($count, $typeAnimal, $typeProduct, $min, $max)
     {
         for ($i = 0; $i < $count; $i++) {
-            $this->cows[] = new Cow();
-        }
-    }
-
-    public function setChickens($count)
-    {
-        for ($i = 0; $i < $count; $i++) {
-            $this->chickens[] = new Chicken();
-        }
-    }
-
-    public function setResult()
-    {
-        foreach ($this->cows as $cow) {
-            $this->milk += $cow->work();
-        }
-
-        foreach ($this->chickens as $chicken) {
-            $this->eggs += $chicken->work();
+            $this->animals[$typeAnimal][] = new Animal($typeAnimal, $typeProduct, $min, $max);
         }
     }
 
     public function showResult()
     {
-        echo 'коровки дали ' . $this->milk . ' литров молочка';
-        echo '<br>';
-        echo 'курочки снесли ' . $this->eggs . ' яичек';
+        foreach ($this->animals as $typeAnimal) {
+            $harvest = 0;
+            foreach ($typeAnimal as $animal) {
+                $harvest += $animal->work();
+            }
+            echo $typeAnimal[0]->typeAnimal . ' - ' .
+                $typeAnimal[0]->typeProduct . ' - ' . $harvest . '<br>';
+        }
+
     }
 }
 
 $bobsFarm = new Farm();
-$bobsFarm->setCows(10);
-$bobsFarm->setChickens(20);
-$bobsFarm->setResult();
+$bobsFarm->setAnimals(20, 'корова', 'молоко', 8, 12);
+$bobsFarm->setAnimals(10, 'корова', 'молоко', 5, 9);
+$bobsFarm->setAnimals(12, 'курачка', 'яйца', 0, 2);
 $bobsFarm->showResult();
